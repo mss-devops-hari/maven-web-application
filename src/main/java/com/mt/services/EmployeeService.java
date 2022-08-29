b087ac581755aef9336321fd8236b000acc84ca0
Nara Hari Babu <kbnarahari432@gmail.com>
2022-08-28 20:07:08 -0400

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,10 +22,10 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
+		js.put("Name", "Hari Technologies");
+		js.put("Calling Name", "Nara Hari");
 		js.put("DOB", "08-Nov-2011");
-		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
+		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping needy People..");
 		js.put("Places he like", "His native place");
 
 		return js.toString();
